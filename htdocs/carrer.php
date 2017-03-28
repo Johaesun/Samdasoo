8ea161469375821b85da6a6721b2f61de1bdff73
Johaesun <991015a@naver.com>
2017-03-28 16:28:25 +0900

    a

--- htdocs/carrer.php
+++ htdocs/carrer.php
@@ -21,13 +21,32 @@
     	       <source src="video/band.mp4" type="video/mp4">
             </video>
     	       <div class="top_text">
-    	           <h2>밴드,고맙삼다</h2>
-    	           <p>브랜드 제주삼다수의 모델</p>
+    	           <h2>브랜드 삼다수</h2>
+    	           <p>세계적인 브랜드로 자리잡는 삼다수</p>
     	       </div>
     	   </div>
     	
     	<div class="box01">
-           <ul class="career">
+            <h2>밴드 고맙삼다</h2>
+            <img src="./img/bottle_3.png" class="bottle" alt="삼다수 아이콘">
+            <ul>
+                <li>
+                    <p>의도를 하나의 단어로 만들어 공유해보면 어떨까라는 생각이 들었어요.</p>
+                    <p>여러 아이디어 가운데 ‘고맙습니다+삼다수’가 제주도 방언스럽기도 하고,</p>
+                    <p>귀여운 맛이 이번 광고 콘셉트와도 잘 어울렸습니다.</p>
+                    </li>
+                <li>
+                    <p>제주삼다수를 좀 더 가깝게 만드는 작업을 영상 콘티 구성에 적극 반영했습니다.</p>
+                    <p> 이를 통해 제주도의 매력을 제주삼다수로 자연스럽게 링크시키면서</p>
+                    <p>새롭게 관심이 가도록 ‘브랜드 리프레시먼트’를 캠페인 목표로 잡았습니다.</p>
+                </li>
+            </ul>
+            <img src="./img/bottle_4.jpg" class="bottle_img" alt="삼다수 이미지">
+            <p class="plus_text">고맙삼다 조물주와의 인터뷰 중</p>
+        </div>
+        
+        <div class="box02">
+            <ul class="career">
                 <li>
                     <img src="./img/nbci.jpg" alt="한국생산성본부"><p>2015 국가브랜드 경쟁력지수(NBCI) 9년 연속 1위 (생수부문,한국생산성본부)</p>
                 </li>
@@ -44,8 +63,9 @@
                     <p>한국에서 가장 존경받는 기업 (먹는샘물(생수)산업부문 1위,한국농물협회컨설팅)</p>
                 </li>
             </ul>
-            
-    	    <ul class="content">
+        </div>
+        <div class="box03">
+            <ul class="content">
     	        <div class="text_box">
     	            <h3>제주 지하수의 브랜드 시대</h3>
     	            <p>물허벅시대 1960년 이전부터</p>
@@ -57,13 +77,6 @@
     	            <p>고품질 지하수로 인정받았습니다.</p>
     	        </div>
     	    </ul>
-        </div>
-        
-        <div class="box02">
-            
-        </div>
-        <div class="box03">
-            
         </div>
         <div class="box04">
             
