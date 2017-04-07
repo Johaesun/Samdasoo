d4ae70ebe3d16811d544a15842e3f3a8cf7967b5
Johaesun <991015a@naver.com>
2017-04-07 11:33:42 +0900

    170407

--- htdocs/js/news.js
+++ htdocs/js/news.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $(".notice_btn").click(function(){
         $(".content_box").addClass("click_off");
+        $(".content_box").addClass("content_box_on");
     });
     $(".close_btn").click(function(){
-        alert();
+        $(".content_box").addClass("click_off");
     });
 });
